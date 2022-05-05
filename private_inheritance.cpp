effdd108cab858c31a2406b16cd9435c9c18242a
CoolD69 <parthasingharoy166@gmail.com>
2022-05-05 13:13:27 +0530

    CPP

--- private_inheritance.cpp
+++ private_inheritance.cpp
@@ -1,14 +1,92 @@
 #include<iostream>
+//#include<cstring>
+#include<string.h>
 using namespace std;
-class A{
-	int a;
+
+class media{
+	
+	protected:
+	   char title[50];
+	   float price;
+	   
 	public:
-			int b;
-			void get_ab();
-			void get_a
+			//constructor
+			media(char *s, float a){
+				strcpy(title,s);
+				price=a;
+			}
+			
+	virtual void display(){
+		
+	}			
+};
+//inhritance
+class books:public media{
+	int pages;
+	public:
+		books(char *s,float a, int p):media(s,a){
+			pages=p;
+		}
+		void display();	
 };
 
+class tape : public media{
+	float time;
+	public:
+		tape(char *s,float a, float t):media(s,a){
+			time = t;
+		}
+		void display();
+};
+
+void books :: display(){
+	cout<<"\n Title "<<title<<endl;
+	cout<<"\n Pages "<<pages<<endl;
+	cout<<"\n Price "<<price<<endl;
+}
+
+void tape :: display(){
+	cout<<"\n Title "<<title<<endl;
+	cout<<"\n Play Time "<<time<<endl;
+	cout<<"\n Price "<<price<<endl;
+}
+
+
+
 int main(){
+	
+	char *title = new char[50];
+	float price,time;
+	int pages;
+	
+	//book details:
+	cout<<"\n Enter the Book Details"<<endl;
+	cout<< "Title : "; cin>>title;
+	cout<< "Price : "; cin>>price;
+	cout<< "Pages : "; cin>>pages;
+	//creating book obj
+	books book1(title,price,pages);
+	
+	//Tape details
+	cout<< "\n \n Enter tape details "<<endl;
+	cout<< "Title : "; cin>>title;
+	cout<< "Price : "; cin>>price;
+	cout<< "Play Time : "; cin>>time;
+	//create a tape obj
+	tape tape1(title,price,time);
+	
+	media *list[2];
+	list[0]=&book1;
+	list[1]=&tape1;
+	
+	//media details
+	cout<<"MEDIA DETAILS"<<endl;
+	
+	cout<<"----------Book---------"<<endl;
+	list[0]->display();
+	
+	cout<<"----------Tape---------"<<endl;
+	list[1]->display();
 	
 	
 		   
--- test.cpp
+++ test.cpp
@@ -1,11 +1,19 @@
-#include<iostream>
-using namespace std;
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+  int *names[5];  
+  
+for(int i=0;i<5;i++)  
+    {  
+        std::cin >> *names[i];  
+    }
 
-int main(){
-	int a;
-	cin >>"Enter the number : ">>a;
+for(int i=0;i<5;i++)  
+    {  
+        std::cout << names[i] << std::endl;  
+    }
+
 	
-		cout<<"The number is "<<a;
-	
-	return 0;
-}
+    return 0;  
+} 
